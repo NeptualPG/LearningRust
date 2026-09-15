013501cdcdd658ac8329cdd0931f0237d8025b2b
Neptual° (Diego Pino) <desarrolladorrpa2@dsinno.io>
2026-09-14 23:54:38 -0500

    loop and mutable

--- fifth/rust-live-5-control-flow/src/main.rs
+++ fifth/rust-live-5-control-flow/src/main.rs
@@ -1,24 +1,14 @@
 fn main(){
-     enum Coin {
-        Penny,
-        Nickel,
-        Dime,
-        Quarter,
-     }
 
-     // we can use the enum to create a varaible
+    let counter = 0; 
 
-     fn value_in_cents(coin: Coin) -> u8{ //u8 is an unsigned 8-bit integer
-         match coin {
-             Coin::Penny => 1,
-             Coin::Nickel => 5,
-             Coin::Dime => 10, // you can't delete the Dime here because that is part of the enum, if you delete it, the code will not compile for pattern matching
-             Coin::Quarter => 25,
-         }
-     }
+    let result = loop{
+        counter +=1;
 
-     let conin = Coin::Penny;
-
-     println!("The value of the coin is: {}", value_in_cents(conin));
-
+        if counter == 10{
+            break counter * 2;
+        }
+    };
+    
+    println!("The result is: {}", result);
 }
