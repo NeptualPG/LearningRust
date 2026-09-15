6ea08f8f28c85f6dad2de73fcf69bccbcfc1f267
Neptual° (Diego Pino) <desarrolladorrpa2@dsinno.io>
2026-09-14 23:40:10 -0500

    conditional rust

--- fifth/rust-live-5-control-flow/src/main.rs
+++ fifth/rust-live-5-control-flow/src/main.rs
@@ -1,44 +1,12 @@
-// fn main(){
-//     let number = 6;
+fn main(){
+    let condition = true;
 
-//     if number < 5 {
-//         println!("condition was true");
-//     } else {
-//         println!("condition was false");
-//     }
+    let number = if condition {
+        5
+    } else {
+        6
+    };
 
-// }
-
-
-// fn main(){
-//     let condition = true;
-
-//     let number = if condition {
-//         5
-//     } else {
-//         6
-//     };
-
-//     println!("The value of number is: {}", number);
-// }
-
-
-// fn main () {
-//     let num = 15;
-
-//     if num % 2 == 0{
-//         println!("{} is even", num);
-//     } else {
-//         println!("{} is odd", num);
-
-//     };
-
-//     if num > 10 {
-//         println!("{} is greater than 10", num);
-//     } else {
-//         println!("{} is less than or equal to 10", num);
-//     };
-//  }
-
-
+    println!("The value of number is: {}", number);
+}
 
