68b028e3512cc1fea0ec57a93e8f52d9828b8914
Neptual° (Diego Pino) <desarrolladorrpa2@dsinno.io>
2026-09-15 00:08:35 -0500

    loop iteration

--- fifth/rust-live-5-control-flow/src/main.rs
+++ fifth/rust-live-5-control-flow/src/main.rs
@@ -2,8 +2,23 @@ fn main( ){
     let a = [1, 2, 3, 4, 5];
 
     // iter() IS A FUCNTION TO RETURN AN ITERATOR OVER THE ARRAY 
-
+    // the element is auto assigned as let and mut to the type of the array, in this case i32, that is crazy
     for element in a.iter() {
-        println!("the value is: {}", element);
-    }
+        println!("the value is: {}", element);   
+    };
+
+    let s = "hellouda";
+
+    // chars() String function returns an iterator over the characters of the string
+
+    for c in s.chars() {
+        println!("the value is: {}", c);
+    };
+
+
+    for number in 1..4{
+        println!("the value is: {}", number);
+    };
+
+
 }
