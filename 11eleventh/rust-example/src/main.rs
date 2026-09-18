08d76f23c27f1123085201272d4c69e72697bfde
NeptualPG <183859970+NeptualPG@users.noreply.github.com>
2026-09-18 17:29:29 -0500

    class: other into a method

--- 11eleventh/rust-example/src/main.rs
+++ 11eleventh/rust-example/src/main.rs
@@ -13,18 +13,37 @@ impl Rectangle{
     fn width(&self) -> bool {
         self.width > 0
     }
+    
+    //getter for height
+    fn height(&self) -> bool {
+        self.height > 0
+    }
+
+    // check if a rectangle ccan hold another
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height 
+    }
 }
 
 fn main(){
     let rect1 = Rectangle {
-        width: 0,
+        width: 31,
         height: 50,
     }; 
+    let rect2 = Rectangle {
+        width: 30,
+        height: 40,
+    }; 
+    let rect3 = Rectangle {
+        width: 60,
+        height: 45,
+    }; 
     // print the area
-    if rect1.width() {
-        println!("The areaa of rectangle is {} square pixels.", rect1.area());
-    } else {
-        println!("ERROR The width of the rectangle is INVALID.");
-    }
+
+    // println!("The height of the rectangle is {}", rect1.height());
+
+    println!("Can rect1 hold react2? {}", rect1.can_hold(&rect2));
+    println!("Can rect1 hold react3? {}", rect1.can_hold(&rect3));
+
 }
 
