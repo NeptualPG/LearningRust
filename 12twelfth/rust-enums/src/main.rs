958c7f9f083be651e37526f14678f86c141de559
NeptualPG <183859970+NeptualPG@users.noreply.github.com>
2026-09-19 20:22:43 -0500

    class: initial commit

---
+++ 12twelfth/rust-enums/src/main.rs
@@ -0,0 +1,21 @@
+// Enums in rust 
+
+// Enums are a way to define a type bu enumerating its possible variants. 
+//  Enums defined using the "enum" keyword.
+// they can contain data as well
+
+// first example with IP adress
+
+enum IpAddrKind{
+    V4,
+    V6
+}
+
+fn main() {
+
+    let four = IpAddrKind::V4;
+    let six = IpAddrKind::V6;
+    
+
+    print("IP address kind: {:?} and {:?}", four, six);
+}
