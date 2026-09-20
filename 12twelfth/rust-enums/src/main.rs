196c3ad782a60d9032c394b50ed797b32dfa37ca
NeptualPG <183859970+NeptualPG@users.noreply.github.com>
2026-09-20 17:16:32 -0500

    feat: enum structured

--- 12twelfth/rust-enums/src/main.rs
+++ 12twelfth/rust-enums/src/main.rs
@@ -1,17 +1,16 @@
 #[derive(Debug)]
 enum IpAddrKind{
-    V4(String),
-    V6(String)
+    V4(u8, u8, u8, u8),
+    V6(String),
 }
 
 
 fn main() {
 
-    
-    let home = IpAddrKind::V4(String::from("127.0.0.1"));
-    let loopback = IpAddrKind::V6(String::from("::1"));
-
+    // Create instances of the enum
+    let home = IpAddrKind::V4(127, 0, 0, 1);
     println!("{:?}", home);
+    
+    let loopback = IpAddrKind::V6(String::from("::1"));
     println!("{:?}", loopback); 
-
 }
