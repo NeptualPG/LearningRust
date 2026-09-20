2b95e0f84ef5d0e8f1935d4dc7162567be4aa0ac
NeptualPG <183859970+NeptualPG@users.noreply.github.com>
2026-09-20 18:36:10 -0500

    class: optional -> Null

--- 12twelfth/rust-enums/src/code.md
+++ 12twelfth/rust-enums/src/code.md
@@ -186,3 +186,42 @@ fn main(){
     c.call();
     
 }
+
+--------------------------
+
+
+// implementation with enum
+
+#[derive(Debug)]
+enum Message {
+    Quit, 
+    Move {x: i32, y: i32},
+    Write(String),
+    ChangeColor(i32, i32, i32),
+}
+
+// method for the generic enum Message
+
+impl Message {
+    fn call(&self) {
+        println!("Message is: {:?}", self);
+    }
+}
+
+fn main(){
+    let m = Message::Write(String::from ("Hello, World!"));
+    let x = Message::Move { x: 3, y: 4 };
+    let y = Message::ChangeColor(0,0,0);
+    let z = Message::Quit;
+
+    // println!("{:?}", m);
+    // println!("{:?}", x);
+    // println!("{:?}", y);
+    // println!("{:?}", z);
+
+    m.call();
+    x.call();
+    y.call();
+    z.call();
+}
+
--- 12twelfth/rust-enums/src/main.rs
+++ 12twelfth/rust-enums/src/main.rs
@@ -1,35 +1,17 @@
-// implementation with enum
+//In rust there is no "Null" value, instead we have "Option" enum
 
-#[derive(Debug)]
-enum Message {
-    Quit, 
-    Move {x: i32, y: i32},
-    Write(String),
-    ChangeColor(i32, i32, i32),
-}
+// enum Option<T> {
+//     Some(T),
+//     None,
+// }
 
-// method for the generic enum Message
+fn main() {
+    let some_number = Some(5);
+    let some_string = Some("Hello");
+    let absent_number: Option<i32> = None;
 
-impl Message {
-    fn call(&self) {
-        println!("Message is: {:?}", self);
-    }
-}
-
-fn main(){
-    let m = Message::Write(String::from ("Hello, World!"));
-    let x = Message::Move { x: 3, y: 4 };
-    let y = Message::ChangeColor(0,0,0);
-    let z = Message::Quit;
-
-    // println!("{:?}", m);
-    // println!("{:?}", x);
-    // println!("{:?}", y);
-    // println!("{:?}", z);
-
-    m.call();
-    x.call();
-    y.call();
-    z.call();
+    println!("{:?}", some_number);
+    println!("{:?}", some_string);
+    println!("{:?}", absent_number);
 }
 
