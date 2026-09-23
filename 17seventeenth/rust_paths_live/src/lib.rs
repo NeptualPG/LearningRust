d5f9384cd80f833692a100cd67aeb4f929f35e59
NeptualPG <183859970+NeptualPG@users.noreply.github.com>
2026-09-23 18:22:35 -0500

    class: inicializing paths class

---
+++ 17seventeenth/rust_paths_live/src/lib.rs
@@ -0,0 +1,14 @@
+// Paths 
+
+mod front_of_house {
+    mod hosting {
+        fn add_to_waitlist(){}
+    }
+}
+
+pub fn eat_at_resyaurant(){
+    //Absolute path
+    crate::front_of_house::hosting::add_to_waitlist();
+    //Relative path
+    front_of_house::hosting::add_to_waitlist();
+}
