c8fe618d5bcbaf080919be2801a116fe9f6c99ce
NeptualPG <183859970+NeptualPG@users.noreply.github.com>
2026-09-22 23:57:28 -0500

    class: already create modules

--- 16sixteenth/rust-modules/src/main.rs
+++ 16sixteenth/rust-modules/src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+use create::garden::vegetables::Asparagus;
+
+pub mod garden;
+
+fn main (){
+    let plant: Aspargus = Asparagus {
+        name: String::from("Asparagus"),
+        stalks: 5,
+    };
+
+    println!("This plant is a {:?}", plant);
 }
