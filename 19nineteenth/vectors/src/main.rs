974f0b77e8baad43831f35d3d3521e2bfa3f39a5
NeptualPG <183859970+NeptualPG@users.noreply.github.com>
2026-09-25 16:01:00 -0500

    class: init clas 19

---
+++ 19nineteenth/vectors/src/main.rs
@@ -0,0 +1,12 @@
+/*Collections are data structures that store multiple values
+
+they are stored on the heap:
+the data does not need to be known at compile time
+and can grow or shrink as the program runs.
+
+A vector allow you to store a variable number of values next to each other in memory.
+ */
+
+fn main() {
+    
+}
