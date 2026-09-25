19e10351cfb4e6321ea0c62c21a70158ed73db17
NeptualPG <183859970+NeptualPG@users.noreply.github.com>
2026-09-25 15:55:14 -0500

    class: waves to import

--- 18eighteenth/rust-use/src/main.rs
+++ 18eighteenth/rust-use/src/main.rs
@@ -1,33 +1,13 @@
-//main.rs
-// use external packages
+// main.rs
+// use nested paths to clean up larger use lists
 
-//dependencies are specified in Cargo.toml file
-use rand; // for random number generation
 
-// std is the stadard library of rust
-// Include: 
-// std::collections::HashMap; // for using HashMap
-// std::io; // for input and output
-// std::fs; // for file system operations
-// std::env; // for environment variables
-// std::thread; // for multithreading
-// std::time; // for time operations
-// std::sync; // for synchronization primitives
-// std::net; // for networking
-// std::process; // for process management
-// std::path; // for path operations
-// std::str; // for string operations
-// etc...
-use std::collections::HashMap; // for using HashMap
+use std::collections::*;
 
-fn main(){
-    // old way of generating a random number
-    // let secret_number = rand::thread_rng().gen_range(1..=100); // generate a random number between 1 and 100
-    let secret_number = rand::random_range(1..=100);
-    println!("The secret number is: {}", secret_number);
-    
-    let mut map = HashMap::new();
-    map.insert(1, 2);
+use::std::io::Write;
+// use::std::io::{self, Write};
 
-    println!("The map is: {:?}", map);
-}
+// Different import ways
+// use std::{cmp::Ordering, io};
+use::std::cmp::Ordering;
+use::std::io;
