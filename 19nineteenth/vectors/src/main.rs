e69b0c59e5a8252570a9c5b4322d67d6a221ceca
NeptualPG <183859970+NeptualPG@users.noreply.github.com>
2026-09-25 16:26:46 -0500

    class: different types into a vector

--- 19nineteenth/vectors/src/main.rs
+++ 19nineteenth/vectors/src/main.rs
@@ -41,5 +41,36 @@ fn main() {
         Some(&fifth) => println!("The fifth element is: {}", fifth),
         None => println!("There is no fifth element."),
     }
- 
+
+    let mut vec6 = vec![10, 20, 30, 40, 50];
+    
+
+    for i in &mut vec6 {
+        // * before i to dereference the mutable reference and get the value it points to
+        *i += 1;
+    }
+
+    println!("vec6 after incrementing each element: {:?}", vec6);
+    
+
+    // if you wanna store different types in a vector using enums
+    #[derive(Debug)]
+    #[allow(dead_code)]
+    enum SpreadsheetCell {
+        Int(i32),
+        Float(f64),
+        Text(String),
+    }
+
+    // is the same type but contains different types of data
+    // vec! is for creating a vector with different types of data using the enum SpreadsheetCell
+    let row = vec![
+        SpreadsheetCell::Int(3),
+        SpreadsheetCell::Float(10.12),
+        SpreadsheetCell::Text(String::from("blue")),
+    ];
+
+    println!("row: {:?}", row);
+
+
 }
